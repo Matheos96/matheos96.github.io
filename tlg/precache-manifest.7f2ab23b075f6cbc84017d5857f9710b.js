21c02cc57cc96c8e664a65331e809469ccf6707e
Matheos Mattsson <matheos.mattsson@gmail.com>
2020-04-20 16:22:32 +0300

    added tlg

---
+++ tlg/precache-manifest.7f2ab23b075f6cbc84017d5857f9710b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a1c116d41f21fa0d943",
+    "url": "/tlg/static/css/main.92a859a9.chunk.css"
+  },
+  {
+    "revision": "0a1c116d41f21fa0d943",
+    "url": "/tlg/static/js/main.c319ad80.chunk.js"
+  },
+  {
+    "revision": "44135705009ff22b4c88",
+    "url": "/tlg/static/js/runtime~main.cecef06e.js"
+  },
+  {
+    "revision": "144d94bbb65850e36a38",
+    "url": "/tlg/static/js/2.a5e0b94e.chunk.js"
+  },
+  {
+    "revision": "ec758963075f7322690a8bade5d2ab52",
+    "url": "/tlg/index.html"
+  }
+];
